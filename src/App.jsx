623f919d3419e69f7c035822645dd57ce77580ce
Muharram Asad <asadmuharram@gmail.com>
2018-05-06 17:28:54 +0700

    add get spotify access_token

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,20 @@ class App extends Component {
     search() {
         console.log('this state ', this.state);
         const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
-        console.log('FETCH URL',FETCH_URL);
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`; //using template string
+        var accessToken = 'BQAtsFmvdP0tH0DzIuSVGg-Zxdpze3iG_uEgqd6S7JojJOF-PyRwz_7JCaWW20JVinBVAp00gwtoluuutNyVsXh6L2g1TZu3FQwjK2KEf3Xa79FyPSNA7t9UB0tCc2EWQLATH8r4aBKyT6YY6YjIWTj3Ia1rADtLgIK2Zvnnp3puRY7Xiw&refresh_token=AQAdRTlM3O2TXxSzCotfqTMHNs7I1skAApt5CGG52Tml8WU5mxK8BSORKiR4xBaQTO_IQ_A8xTeRuN8ePt9qlnl1sBumM0c8-RN-5qhiVZFdpddSDpL2H5Qfbi0e7_Q4oI0';
+        var myHeaders = new Headers();
+        var myOptions = {
+        method: 'GET',
+        headers:  {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        mode: 'cors',
+        cache: 'default'
+        };
+        fetch(FETCH_URL, myOptions )
+        .then(response => response.json())
+        .then(json => console.log(json))
     }
 
     render() {
